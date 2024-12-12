7d069cef7769bc04d4eb8986652e48135766cc76
Daffa Rizq <daimenrizq7@gmail.com>
2024-12-12 22:47:50 +0700

    feat: change database pool to connection string

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,12 +3,19 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+// const pool = new Pool({
+//   user: process.env.PG_USER || "postgres",
+//   host: process.env.PG_HOST || "localhost",
+//   database: process.env.PG_DATABASE || "nutech",
+//   password: process.env.PG_PASSWORD || "root",
+//   port: parseInt(process.env.PG_PORT as string) || 5432,
+// });
+
 const pool = new Pool({
-  user: process.env.PG_USER || "postgres",
-  host: process.env.PG_HOST || "localhost",
-  database: process.env.PG_DATABASE || "nutech",
-  password: process.env.PG_PASSWORD || "root",
-  port: parseInt(process.env.PG_PORT as string) || 5432,
+  connectionString: process.env.DATABASE_URL || undefined,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
